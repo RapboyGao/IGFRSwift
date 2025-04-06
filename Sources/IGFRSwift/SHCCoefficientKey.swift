4ebdb86639869025a9ed68bcfee7834bc75f59dd
RapboyGao <atgao74201@qq.com>
2025-04-06 20:55:47 +0800

    2025.04.06 添加SHCCoefficientKey结构的注释，明确球谐函数阶数、系数类型及其枚举

--- Sources/IGFRSwift/SHCCoefficientKey.swift
+++ Sources/IGFRSwift/SHCCoefficientKey.swift
@@ -1,8 +1,11 @@
 import Foundation
 
 public struct SHCCoefficientKey: Codable, Hashable, Sendable {
+    /// 球谐函数阶数（degree），取值范围 1 ≤ n ≤ 模型最大阶数
     public let n: Int
+    /// 系数类型（g或h），当m=0时只有g类型
     public let m: Int
+    /// 球谐系数类型枚举
     public let type: SHCCoefficientType
 
     public init(n: Int, m: Int, type: SHCCoefficientType) {
