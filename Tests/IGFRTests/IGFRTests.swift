50122b04cd78c2386e62a84f7be78ca270e8da19
RapboyGao <atgao74201@qq.com>
2025-09-03 12:33:09 +0800

    2025.09.03 feat: 添加磁场计算结果结构体
    实现 MagneticFieldResult 结构体用于存储和表示地磁场计算结果，包含XYZ分量、总强度、磁倾角、磁偏角等属性。该结构体支持 Codable、Hashable 等协议，便于序列化和比较。

--- Tests/IGFRTests/IGFRTests.swift
+++ Tests/IGFRTests/IGFRTests.swift
@@ -4,5 +4,7 @@ import Testing
 @Test func example() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
 //    let result = try IGRFCalculator.calculate(latitude: 40, longitude: 116, altitude: 1, date: .now)
+//    let model = SHCModel.model14
+//    let result = model.calculateMagneticDeclination(longitude: 40, latitude: 116, altitude: 0, year: 2025)
 //    print(result)
 }
