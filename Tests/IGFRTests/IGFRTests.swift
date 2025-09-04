fe7fa4f330d2e5b67f224dcd02ab29c28690721e
RapboyGao <atgao74201@qq.com>
2025-09-04 15:19:50 +0800

    2025.09.04 test(IGFRTests): 添加磁力场和IGRF计算测试用例
    添加两个新的测试函数：testMagneticField 和 testIGRF001，分别用于测试磁力场计算结果和IGRF模型计算功能

--- Tests/IGFRTests/IGFRTests.swift
+++ Tests/IGFRTests/IGFRTests.swift
@@ -1,12 +1,14 @@
 @testable import IGFRSwift
+import CoreLocation
 import Testing
 
-@Test func example() async throws {
+@Test func testMagneticField() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-//    let result = try IGRFCalculator.calculate(latitude: 40, longitude: 116, altitude: 1, date: .now)
-//    let model = SHCModel.model14
-//    let result = model.calculateMagneticDeclination(longitude: 40, latitude: 116, altitude: 0, year: 2025)
-//    print(result)
     let result = MagneticFieldResult(latitude: 40, longitude: 116, altitude: 0, north: 27708, east: -3599, up: 47378)
+    print(result)
+}
+
+@Test func testIGRF001() async throws {
+    let result = SHCModel.latest.calculate(CLLocation(latitude: 40, longitude: 116))
     print(result)
 }
