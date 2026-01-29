355a8e6439db1cd7ed8ed165ad6dbcadc217dcb1
RapboyGao <atgao74201@qq.com>
2026-01-29 23:53:16 +0800

    refactor: 移除对swift-numerics的依赖
    不再需要swift-numerics库的功能，因此从Package.swift中移除相关依赖配置并删除Package.resolved文件

--- Package.swift
+++ Package.swift
@@ -13,17 +13,11 @@ let package = Package(
             targets: ["GeoMagSwift"]
         )
     ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-numerics", from: "1.0.3")
-    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "GeoMagSwift",
-            dependencies: [
-                .product(name: "Numerics", package: "swift-numerics")
-            ],
             resources: [.process("Resources")]
         ),
         .testTarget(
