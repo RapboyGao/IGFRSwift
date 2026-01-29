32cdccf1076f17c6e801611e761975655ce50362
RapboyGao <atgao74201@qq.com>
2026-01-29 23:38:16 +0800

    feat(本地化): 添加多语言支持资源文件并设置默认本地化为英文
    添加了多种语言的本地化资源文件，包括语言代码和地区变体
    在Package.swift中设置默认本地化为英文并优化了依赖格式

--- Package.swift
+++ Package.swift
@@ -5,16 +5,16 @@ import PackageDescription
 
 let package = Package(
     name: "GeoMagSwift",
-
+    defaultLocalization: "en",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "GeoMagSwift",
             targets: ["GeoMagSwift"]
-        ),
+        )
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-numerics", from: "1.0.3"),
+        .package(url: "https://github.com/apple/swift-numerics", from: "1.0.3")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -22,7 +22,7 @@ let package = Package(
         .target(
             name: "GeoMagSwift",
             dependencies: [
-                .product(name: "Numerics", package: "swift-numerics"),
+                .product(name: "Numerics", package: "swift-numerics")
             ],
             resources: [.process("Resources")]
         ),
